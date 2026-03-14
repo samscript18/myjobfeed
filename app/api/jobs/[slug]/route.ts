db0bf3dff5d93c8ef7c5ae6adca63169dd2a5871
samscript18 <adeolasam20@gmail.com>
2026-03-14 14:36:33 +0100

    fix: return next response in get single job endpoint

--- app/api/jobs/[slug]/route.ts
+++ app/api/jobs/[slug]/route.ts
@@ -1,17 +1,33 @@
-import dbConnect from '@/lib/db/mongodb';
-import Job from '@/lib/db/models/job';
-import { NextRequest, NextResponse } from 'next/server';
+import dbConnect from "@/lib/db/mongodb";
+import Job from "@/lib/db/models/job";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: Promise<{ slug: string }> },
+) {
 	await dbConnect();
 	const { slug } = await params;
 
 	try {
-		const job = await Job.findOne({ slug: slug as string }).populate('categoryId', 'name slug');
-		if (!job) return NextResponse.json({ success: false, error: 'Job not found' });
-		NextResponse.json({ success: true, message: 'Job retrieved successfully', data: job });
+		const job = await Job.findOne({
+			slug: slug as string,
+		}).populate("categoryId", "name slug");
+		if (!job)
+			return NextResponse.json({
+				success: false,
+				error: "Job not found",
+			});
+		return NextResponse.json({
+			success: true,
+			message: "Job retrieved successfully",
+			data: job,
+		});
 	} catch (err) {
-		console.error('[Job GET] Error:', err);
-		NextResponse.json({ success: false, error: 'Failed to fetch job' });
+		console.error("[Job GET] Error:", err);
+		return NextResponse.json({
+			success: false,
+			error: "Failed to fetch job",
+		});
 	}
 }
--- app/api/route.ts
+++ app/api/route.ts
@@ -9,13 +9,16 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 	// await seedCategories();
-	const arbeitnow = await fetchArbeitnow();
-	const themuse = await fetchTheMuse();
-	const jobicy = await fetchJobicy();
-	const jooble = await fetchJooble();
+	// const arbeitnow = await fetchArbeitnow();
+	// const themuse = await fetchTheMuse();
+	// const jobicy = await fetchJobicy();
+	// const jooble = await fetchJooble();
 
+	// return NextResponse.json({
+	// 	success: true,
+	// 	data: { arbeitnow, themuse, jobicy, jooble },
+	// });
 	return NextResponse.json({
 		success: true,
-		data: { arbeitnow, themuse, jobicy, jooble },
 	});
 }
